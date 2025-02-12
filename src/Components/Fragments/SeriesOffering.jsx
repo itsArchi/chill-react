15a45f5822647ecf5d6d73dff5832f2152139c51
itsArchi <noerrohim10@gmail.com>
2025-02-12 21:18:16 +0700

    fix: arrow series offering pages

--- src/Components/Fragments/SeriesOffering.jsx
+++ src/Components/Fragments/SeriesOffering.jsx
@@ -1,25 +1,98 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { useRef } from "react";
+import Slider from "react-slick";
 import ContainerPremium from "../Elements/Container/ContainerPremium";
 import Arrow from "../Elements/ContinueWatching/Arrow";
 
 const SeriesOffering = () => {
+  const films = [
+    { image: "/assets/list-film/bighero.png" },
+    { image: "/assets/list-film/dontlookup.png" },
+    { image: "/assets/list-film/blackadam.png" },
+    { image: "/assets/list-film/suzume.png" },
+    { image: "/assets/list-film/korea.png" },
+    { image: "/assets/list-film/allofus.png" },
+    { image: "/assets/list-film/dilan.png" },
+    { image: "/assets/list-film/doctorstrange.png" },
+    { image: "/assets/list-film/alice.png" },
+    { image: "/assets/list-film/allofus.png" },
+    { image: "/assets/list-film/batman.png" },
+    { image: "/assets/list-film/baymax.png" },
+    { image: "/assets/list-film/bluelock.png" },
+    { image: "/assets/list-film/devilalltime.png" },
+    { image: "/assets/list-film/bokunohero.png" },
+    { image: "/assets/list-film/jurassic.png" },
+    { image: "/assets/list-film/guardian.png" },
+    { image: "/assets/list-film/sonic.png" },
+    { image: "/assets/list-film/duty.png" },
+    { image: "/assets/list-film/fastx.png" },
+    { image: "/assets/list-film/happiness.png" },
+    { image: "/assets/list-film/quantum.png" },
+    { image: "/assets/list-film/spiderman.png" },
+    { image: "/assets/list-film/tedlasso.png" },
+  ];
+
+  const sliderRef = useRef(null);
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    lazyLoad: "ondemand",
+    speed: 700,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    adaptiveHeight: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: false,
+          infinite: true,
+          swipeToSlide: true,
+        },
+      },
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          arrows: false,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className="relative px-[45px] flex overflow-x-scroll sm:overflow-visible sm:justify-center sm:gap-6 gap-4 snap-x snap-mandatory sm:snap-none w-screen sm:w-full min-h-[145px] sm:h-[365px] -mt-2 sm:mt-0">
-      <div className="flex-shrink-0 snap-center sm:flex-shrink">
-        <ContainerPremium image="/assets/list-film/mermaid.png" />
-      </div>
-      <div className="flex-shrink-0 snap-center sm:flex-shrink">
-        <ContainerPremium image="/assets/list-film/duty.png" />
-      </div>
-      <div className="flex-shrink-0 snap-center sm:flex-shrink">
-        <ContainerPremium image="/assets/list-film/bighero.png" />
-      </div>
-      <div className="flex-shrink-0 snap-center sm:flex-shrink">
-        <ContainerPremium image="/assets/list-film/allofus.png" />
-      </div>
-      <div className="flex-shrink-0 snap-center sm:flex-shrink">
-        <ContainerPremium image="/assets/list-film/missing.png" />
-      </div>
-      <Arrow />
+    <div className="relative w-full min-h-[145px] sm:h-[365px]">
+      <Arrow
+        onScrollLeft={() => sliderRef.current.slickPrev()}
+        onScrollRight={() => sliderRef.current.slickNext()}
+      />
+      <Slider ref={sliderRef} {...settings} >
+        {films.map((film, index) => (
+          <ContainerPremium key={index} image={film.image} />
+        ))}
+      </Slider>
     </div>
   );
 };
